b53863fb7255deef7a1ff5b8a53cad6b319fc46a
nils <test@test.com>
2023-09-05 09:38:00 +0200

    all methods for reservations

--- src/main/java/ch/zli/m223/controller/ReservationController.java
+++ src/main/java/ch/zli/m223/controller/ReservationController.java
@@ -2,9 +2,16 @@ package ch.zli.m223.controller;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
@@ -24,4 +31,66 @@ public class ReservationController {
     public List<Reservation> getReservations() {
         return reservationService.getAll();
     }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    @Operation(summary = "delete reservation", description = "deletes reservation by id")
+    public Response deleteReservation(@PathParam("id") Long id) {
+        Reservation existingReservation = reservationService.getReservationById(id);
+
+        reservationService.deleteReservation(existingReservation);
+
+        return Response.status(200, "deleted").build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Operation(summary = "get reservation by id", description = "returns reservation")
+    public Reservation getReservationById(@PathParam("id") Long id) {
+        return reservationService.getReservationById(id);
+    }
+
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
+    public Reservation create(Reservation reservation) {
+       return reservationService.createReservation(reservation);
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "updates reservation", description = "updates reservation")
+    public Response updateUser(@PathParam("id") Long id, Reservation updatedReservation) {
+        Reservation existingReservation = reservationService.getReservationById(id);
+
+        existingReservation.setStatus(updatedReservation.getStatus());
+        existingReservation.setEndDate(updatedReservation.getEndDate());
+        existingReservation.setStartDate(updatedReservation.getStartDate());
+
+        reservationService.updateReservation(existingReservation);
+
+        return Response.status(201, "updated").build();
+    }
+
+
+    @GET
+    @Path("/accepted")
+    @Operation(summary = "get all accepted reservations", description = "get accepted reservations")
+    public List<Reservation> getAcceptedReservations() {
+        return reservationService.getAcceptedReservations();
+    }
+
+    @GET
+    @Path("/user/{id}")
+    @Operation(summary = "get user reservations", description = "returns all reservations of user")
+    public List<Reservation> getUserReservations(@PathParam("id") Long id) {
+        return reservationService.getUserReservations(id);
+    }
+
+
+
 }
--- src/main/java/ch/zli/m223/service/ReservationService.java
+++ src/main/java/ch/zli/m223/service/ReservationService.java
@@ -3,6 +3,7 @@ package ch.zli.m223.service;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import java.util.List;
 
@@ -14,9 +15,41 @@ public class ReservationService {
     EntityManager entityManager;
 
     public List<Reservation> getAll() {
-         var query = entityManager.createQuery("FROM Reservation", Reservation.class);
+        var query = entityManager.createQuery("FROM Reservation", Reservation.class);
         return query.getResultList();
 
+    }
+
+    @Transactional
+    public Reservation getReservationById(Long id) {
+        return entityManager.find(Reservation.class, id);
+    }
+
+    @Transactional
+    public void deleteReservation(Reservation reservation) {
+        entityManager.remove(reservation);
+    }
+
+    @Transactional
+    public void updateReservation(Reservation reservation) {
+        entityManager.merge(reservation);
+    }
+
+    @Transactional
+    public Reservation createReservation(Reservation reservation) {
+        entityManager.persist(reservation);
+        return reservation;
+    }
+
+    public List<Reservation> getAcceptedReservations() {
+        var query = entityManager.createQuery("FROM Reservation WHERE status = 'accepted'", Reservation.class);
+        return query.getResultList();
+    }
+
+    public List<Reservation> getUserReservations(Long id) {
+        var query = entityManager.createQuery("FROM Reservation WHERE id = :id", Reservation.class);
+        query.setParameter("id", id); 
+        return query.getResultList();
     }
 
 }
